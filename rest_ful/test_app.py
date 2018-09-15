8d5f5c58fc4a7a533807723649b815d3b2f42b22
joshtrigger <lugjosh@gmail.com>
2018-09-15 15:44:40 +0300

    unittest file updated

--- rest_ful/test_app.py
+++ rest_ful/test_app.py
@@ -1,28 +1,30 @@
 import os
 from app import app, Orders, Order
 import unittest
-import tempfile
+
 
 class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def tearDown(self):
+        self.client = None
+
     def test_get(self):
-        tester = app.test_client(self)
-        response = tester.get('/api/v1/orders', content_type='')
+        response = self.client.get('/api/v1/orders', content_type='appliation/json')
         self.assertEqual(response.status_code, 200)
 
     def test_post(self):
-        tester = app.test_client(self)
-        response = tester.post('/api/v1/orders', content_type='')
-        self.assertEqual(response.status_code, 500)
+        response = self.client.post('/api/v1/orders', content_type='appliation/json')
+        self.assertEqual(response.status_code, 201)
 
     def test_put(self):
-        tester = app.test_client(self)
-        response = tester.put('/api/v1/orders/<int:orderId>', content_type='')
-        self.assertEqual(response.status_code, 404)
+        response = self.client.post('/api/v1/orders/<int:orderId>', content_type='appliation/json')
+        self.assertEqual(response.status_code, 201)
 
     def test_delete(self):
-        tester = app.test_client(self)
-        response = tester.delete('/api/v1/orders/<int:orderId>', content_type='')
-        self.assertEqual(response.status_code, 404)
+        response = self.client.post('/api/v1/orders/<int:orderId>', content_type='appliation/json')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
