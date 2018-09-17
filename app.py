0cf49dcc403f30a61502104751077cface4ee1b8
joshtrigger <lugjosh@gmail.com>
2018-09-17 17:26:51 +0300

    get all orders

--- app.py
+++ app.py
@@ -12,27 +12,7 @@ class Orders(Resource):
     def get(self):
        return orders
 
-    #Place new order
-    def post(self):
-        pass
-
 api.add_resource(Orders, '/api/v1/orders')
-
-class Order(Resource):
-    #Fetch a specific order
-    def get(self, orderId):
-       pass
-
-    #Update order status and creates new order
-    def put(self, orderId):
-        pass
-
-    #Deletes an order from order list
-    def delete(self, orderId):
-        pass
-        
-         
-api.add_resource(Order, '/api/v1/orders/<int:orderId>')
    
 if __name__ == '__main__':
     app.run(debug=True) #Runs the app
