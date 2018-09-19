a4f4c9c13533203aa4c3b48fd51b5adacd2d2736
joshtrigger <lugjosh@gmail.com>
2018-09-19 20:45:10 +0300

    updated

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 from api.views import app
-from flask import Flask, jsonify
-
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify({'Welcome: Hi there this is my very first Flask-API applcation'})
 
 if __name__ == '__main__':
     app.run(debug = True)
