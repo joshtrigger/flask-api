04c208e79b187fb4c7074a779d9f14af45f72fdf
joshtrigger <lugjosh@gmail.com>
2018-09-19 19:05:17 +0300

    Added heroku deployment required files

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from api.views import app
 
+@app.route('/')
+def home():
+    return jsonify({'Welcome: Hi there this is my very first Flask-API applcation'})
+    
 if __name__ == '__main__':
     app.run(debug = True)
