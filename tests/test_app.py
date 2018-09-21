65bec32ddb160ae3528731b11a9e50cd2ba28403
joshtrigger <lugjosh@gmail.com>
2018-09-21 13:26:10 +0300

    updated tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ import unittest
 class AppTestCase(unittest.TestCase):
     def test_get(self):
         tester = app.test_client(self)
-        response = tester.get('/api/v1/orders', data = dict(), content_type='application/json')
+        response = tester.get()
         if response == tester.get('/api/v1/orders', data = dict(), content_type='application/json') \
                    or \
                    tester.get('/api/v1/orders/<int:orderId>', data = dict(), content_type='application/json'):
@@ -12,18 +12,18 @@ class AppTestCase(unittest.TestCase):
 
     def test_post(self):
         tester = app.test_client(self)
-        response = tester.get('/api/v1/orders', data = dict(), content_type='application/json')
-        self.assertTrue(b'orderId', response.data)
+        response = tester.post('/api/v1/orders', data = dict(), content_type='application/json')
+        self.assertTrue(200, response.status_code)
 
     def test_put(self):
         tester = app.test_client(self)
-        response = tester.get('/api/v1/orders/<int:orderId>', data = dict(), content_type='application/json')
-        self.assertTrue(b'orderId', response.data)
+        response = tester.put('/api/v1/orders/<int:orderId>', data = dict(), content_type='application/json')
+        self.assertTrue(201, response.status_code)
 
     def test_delete(self):
         tester = app.test_client(self)
-        response = tester.get('/api/v1/orders/<int:orderId>', data = dict(), content_type='application/json')
-        self.assertTrue(b'order has been deleted', response.data)
+        response = tester.delete('/api/v1/orders/<int:orderId>', data = dict(), content_type='application/json')
+        self.assertTrue(200, response.status_code)
 
 if __name__ == '__main__':
     unittest.main()
