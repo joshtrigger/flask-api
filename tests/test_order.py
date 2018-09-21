de3c5c0a9e7413777b9c010d6bcc281abbfc4bc8
joshtrigger <lugjosh@gmail.com>
2018-09-21 13:55:30 +0300

    added test order file

---
+++ tests/test_order.py
@@ -0,0 +1,21 @@
+import unittest
+from api import order
+
+class OrderTest(unittest.TestCase):
+    def test_place_new_order(self):
+        pass
+
+    def test_get_all_orders(self):
+        pass
+
+    def test_fetch_specific_order(self):
+        pass
+
+    def test_update_order_status(self):
+        pass
+
+    def test_delete_order(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
