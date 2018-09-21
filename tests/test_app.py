0f3a228ffd4bfd3b582fdcf108e982f0020962ab
joshtrigger <lugjosh@gmail.com>
2018-09-21 19:42:11 +0300

    updated

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,21 +16,23 @@ class AppTestCase(unittest.TestCase):
     def test_get(self):
         response = self.tester.get()
         if response == self.tester.get('/api/v1/orders', data = dict(), content_type='application/json') \
-                   or \
-                   self.tester.get('/api/v1/orders/1', data = dict(), content_type='application/json'):
+                or \
+                self.tester.get('/api/v1/orders/1', data = dict(), content_type='application/json'):
             return self.assertTrue(response.status_code, 200)
 
     def test_post(self):
         response = self.tester.post('/api/v1/orders', data = dict(), content_type='application/json')
         self.assertTrue(201, response.status_code)
+        self.assertTrue(b'[]', response.data)
 
     def test_put(self):
         response = self.tester.put('/api/v1/orders/2', data = dict(), content_type='application/json')
         self.assertTrue(200, response.status_code)
+        self.assertTrue(b'[]', response.data)
 
     def test_delete(self):
         response = self.tester.delete('/api/v1/orders/1', data = dict(), content_type='application/json')
         self.assertEqual(200, response.status_code)
-
+        
 if __name__ == '__main__':
     unittest.main()
