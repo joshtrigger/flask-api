d3d723618536508a064dc9cecccfce8deb3c6a64
joshtrigger <lugjosh@gmail.com>
2018-09-21 14:02:31 +0300

    added test order file

--- tests/test_order.py
+++ tests/test_order.py
@@ -8,13 +8,13 @@ class OrderTest(unittest.TestCase):
     def test_get_all_orders(self):
         pass
 
-    def test_fetch_specific_order(self):
+    def test_fetch_specific_order(self, orderId):
         pass
 
-    def test_update_order_status(self):
+    def test_update_order_status(self, orderId):
         pass
 
-    def test_delete_order(self):
+    def test_delete_order(self, orderId):
         pass
 
 if __name__ == '__main__':
