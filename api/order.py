f752c19c6ef93fb407eac9692f89f05f724442e7
joshtrigger <lugjosh@gmail.com>
2018-09-25 20:07:57 +0300

    first attempt to input validation

--- api/order.py
+++ api/order.py
@@ -2,15 +2,17 @@ from api.views import request, reqparse
 
 class myOrder:
     parser = reqparse.RequestParser()
-    parser.add_argument('orderId', type=int, required=True, help='field cannot be blank')
-    parser.add_argument('name', type=str, required=True, help='field cannot be blank')
-    parser.add_argument('price', type=int, required=True, help='field cannot be blank')
+    parser.add_argument('orderId', type=int, required=True, help='Error: Must be an integer')
+    parser.add_argument('name', type=str, required=True, help='Error: Must be a string')
+    parser.add_argument('price', type=int, required=True, help='Error: Must be an Integer')
 
     def __init__(self):
         """Initialisation"""
         self.orders = []
+        
 
     def place_new_order(self):
+        """places a new order [POST] method"""
         # if next(filter(lambda x:x['orderId'], self.orders), None):
         #     return {'message': "Order already exists."}, 400
 
@@ -25,13 +27,18 @@ class myOrder:
         return order, 201
 
     def get_all_orders(self):
-        return self.orders, 200
+        """places a new order [GET] method"""
+        if self.orders:
+            return self.orders
+        return {'message': 'No orders found'}
     
     def fetch_specific_order(self, orderId):
+        """places a new order [GET] method"""
         self.order = next(filter(lambda x:x['orderId'] == orderId, self.orders), None)
         return self.order, 200 if self.order else 404
 
     def update_order_status(self, orderId):
+        """places a new order [PUT] method"""
         order = next(filter(lambda x:x['orderId'] == orderId, self.orders), None)
 
         data = myOrder.parser.parse_args()
@@ -49,5 +56,6 @@ class myOrder:
         return order, 201
 
     def delete_order(self, orderId):
+        """places a new order [DELETE] method"""
         self.orders = list(filter(lambda x:x['orderId'] != orderId, self.orders))
         return {'message': 'order has been deleted'}, 200
