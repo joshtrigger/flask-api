92f05a6420e26f32004440618f77f888792192b1
joshtrigger <lugjosh@gmail.com>
2018-09-25 12:41:28 +0300

    included some dummy data to tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,12 @@
 from api.views import app
-import unittest
+import unittest, json
 
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
+        app.testing = True
         self.tester = app.test_client()
+        self.order = {'orderId':1, 'name':'pizza', 'price':2500}
 
     def tearDown(self):
         self.tester = None
@@ -15,23 +17,22 @@ class AppTestCase(unittest.TestCase):
 
     def test_get(self):
         response = self.tester.get()
-        if response == self.tester.get('/api/v1/orders', data = dict(), content_type='application/json') \
+        if response == self.tester.get('/api/v1/orders', data = self.order) \
                 or \
-                self.tester.get('/api/v1/orders/1', data = dict(), content_type='application/json'):
+                self.tester.get('/api/v1/orders/1', data = self.order):
             return self.assertTrue(response.status_code, 200)
 
     def test_post(self):
-        response = self.tester.post('/api/v1/orders', data = dict(), content_type='application/json')
-        self.assertTrue(201, response.status_code)
-        self.assertTrue(b'[]', response.data)
-
+        response = self.tester.post('/api/v1/orders', data = self.order)
+        self.assertEqual(201, response.status_code)
+        
     def test_put(self):
-        response = self.tester.put('/api/v1/orders/2', data = dict(), content_type='application/json')
+        response = self.tester.put('/api/v1/orders/2', data = self.order)
         self.assertTrue(200, response.status_code)
-        self.assertTrue(b'[]', response.data)
+        
 
     def test_delete(self):
-        response = self.tester.delete('/api/v1/orders/1', data = dict(), content_type='application/json')
+        response = self.tester.delete('/api/v1/orders/1', data = self.order)
         self.assertEqual(200, response.status_code)
         
 if __name__ == '__main__':
