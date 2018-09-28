3861a8e962a5e6881cb9c2c3378728cd07e851eb
joshtrigger <lugjosh@gmail.com>
2018-09-28 14:29:23 +0300

    order status input validation

--- api/order.py
+++ api/order.py
@@ -47,16 +47,16 @@ class myOrder:
         #data = myOrder.parser.parse_args()
         status_data = request.get_json()
 
-        if order:
+        if order is None:
             status = status_data['status']
-            # for entry in status:
-            if status.isspace():
-                return {'message': 'Field cannot be blank'}, 400
-            else:
-                order = {'status': status}
-                self.orders.append(order)
-        
-        order.update(status_data)
+            for entry in status:
+                if entry.isspace():
+                    return {'message': 'Field cannot be blank'}, 400
+                else:
+                    order = {'status': status}
+                    self.orders.append(order)
+        else:
+            order.update(status_data)
         return order, 201
 
     def delete_order(self, orderId):
