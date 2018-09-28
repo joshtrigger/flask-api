f0f3cbe267d8827a8a1b6bebe691e9e1d091b82f
joshtrigger <lugjosh@gmail.com>
2018-09-28 16:35:53 +0300

    order status input validation

--- api/order.py
+++ api/order.py
@@ -52,7 +52,7 @@ class myOrder:
             if status.isspace():
                 return {'message': 'Field cannot be blank'}, 400
             
-            order = {'status': status}
+            order = {'name':'','status': status}
             order.update(data)
             self.orders.append(order)
         return order, 201
