7b0b1df643b8e9df382295a20d21c573ba957e0c
joshtrigger <lugjosh@gmail.com>
2018-09-28 12:21:00 +0300

    order status input validation

--- api/order.py
+++ api/order.py
@@ -44,11 +44,11 @@ class myOrder:
     def update_order_status(self, orderId):
         """updates the order status [PUT] method"""
         order = next(filter(lambda x:x['orderId'] == orderId, self.orders), None)
-        data = myOrder.parser.parse_args()
-        status_data = request.get_json()
+        #data = myOrder.parser.parse_args()
+        data = request.get_json()
 
         if order is None:
-            status = status_data['status']
+            status = data['status']
 
             for x in status:
                 if x.isspace():
