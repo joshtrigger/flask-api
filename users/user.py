b443b9793f541b7a723f65e23e81748a4ac0aaa7
joshtrigger <lugjosh@gmail.com>
2018-09-29 17:18:19 +0300

    updated

--- users/user.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self,_id, username, email, password):    
-        self.id = _id
-        self.username = username
-        self.email = email
-        self.password = password
