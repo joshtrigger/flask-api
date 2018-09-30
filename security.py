83f0cf117248d9546b71ce409de3182b5ab273f4
joshtrigger <lugjosh@gmail.com>
2018-09-30 11:31:46 +0300

    jwt authentication

--- security.py
+++ security.py
@@ -6,7 +6,7 @@ users = [User(1, 'joshua', 'qwerty')]
 username_mapping = {u.username: u for u in users}
 userId_mapping = {u.id: u for u in users}
 
-def authenticate(username, email, password):
+def authenticate(username, password):
     user = username_mapping.get(username, None)
     if user and safe_str_cmp(user.password, password):
         return user
