e0087cebff8447fb7d217f3d9f6928af8164630f
joshtrigger <lugjosh@gmail.com>
2018-10-02 01:10:23 +0300

    modified order table

--- mydatabase.py
+++ mydatabase.py
@@ -36,6 +36,8 @@ class Database:
             orderId SERIAL PRIMARY KEY,
             userId INTEGER NOT NULL,
             foodId INTEGER NOT NULL,
+            name VARCHAR,
+            price BIGINT NOT NULL,
             status VARCHAR DEFAULT 'Pending',
             FOREIGN KEY (userId)
                 REFERENCES users (userId)
