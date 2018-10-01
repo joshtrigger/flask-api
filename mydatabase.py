5c50d4143e85a31ed7afaa73409d7079fd0de576
joshtrigger <lugjosh@gmail.com>
2018-10-01 18:03:57 +0300

    database file

--- mydatabase.py
+++ mydatabase.py
@@ -8,35 +8,49 @@ class Database:
                 database = "fastfood", host = "localhost", user = "postgres",
                 password = "mypostgres", port = "5432"
             )
+            self.connection.autocommit = True
             self.cursor = self.connection.cursor()
         except:
             pprint('cannot connect to database')
 
     def create_user_table(self):
-        create_table = """CREATE TABLE users(userId SERIAL PRIMARY KEY, 
+        create_table = """CREATE TABLE IF NOT EXISTS users(
+            userId SERIAL PRIMARY KEY, 
             username VARCHAR, email VARCHAR,
             password VARCHAR)"""
         self.cursor.execute(create_table)
         self.connection.commit()
 
-    def create_order_table(self):
-        create_table = """CREATE TABLE orders(orderId SERIAL PRIMARY KEY, 
-            userId INTEGER REFERENCES menu(userId),
-            foodId INTEGER REFERENCES menu(foodID),
-            name VARCHAR, price INTEGER)"""
+    def create_menu_table(self):
+        create_table = """CREATE TABLE IF NOT EXISTS menu(
+            foodId SERIAL PRIMARY KEY,
+            name VARCHAR,
+            description VARCHAR,
+            price INTEGER NOT NULL)"""
         self.cursor.execute(create_table)
         self.connection.commit()
 
-    def create_menu_table(self):
-        create_table = """CREATE TABLE menu(foodId SERIAL PRIMARY KEY,
+    def create_order_table(self):
+        create_table = """CREATE TABLE IF NOT EXISTS orders(
+            orderId SERIAL PRIMARY KEY,
+            userId INTEGER NOT NULL,
+            foodId INTEGER NOT NULL,
             name VARCHAR,
-            description VARCHAR,
-            price INTEGER)"""
+            price BIGINT NOT NULL,
+            status VARCHAR DEFAULT 'Pending',
+            FOREIGN KEY (userId)
+                REFERENCES users (userId)
+                ON UPDATE CASCADE ON DELETE CASCADE,
+            FOREIGN KEY (foodId)
+                REFERENCES menu (foodId)
+                ON UPDATE CASCADE ON DELETE CASCADE
+        )"""
         self.cursor.execute(create_table)
         self.connection.commit()
 
 if __name__ == '__main__':
     database = Database()
     database.create_user_table()
-    database.create_order_table()
     database.create_menu_table()
+    database.create_order_table()
+    
