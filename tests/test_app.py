9baeaebeec054d754899f74860023a5d145963bc
joshtrigger <lugjosh@gmail.com>
2018-10-03 12:38:05 +0300

    updated initial test file

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,14 @@
 from api.views import app
 from mydatabase import Database
-import unittest, json
+import unittest
+import json
 
 class AppTestCase(unittest.TestCase):
-
     def setUp(self):
         """Initialisez app and defines variables"""
         app.testing = True
         self.tester = app.test_client()
-        self.order = {'orderId':1, 'userId':1, 'foodId':1, 'status':'pending'}
+        self.order = {'orderId': 1, 'userId': 1, 'foodId': 1, 'status': 'pending'}
 
     def tearDown(self):
         """Crashes down all initialized variables"""
@@ -20,36 +20,39 @@ class AppTestCase(unittest.TestCase):
 
     def test_get(self):
         """Tests api to get all orders"""
-        response = self.tester.get('/api/v1/orders', data = self.order)
+        response = self.tester.get('/api/v1/orders', data=self.order)
         self.assertEqual(200, response.status_code)
 
         """Tests api to get a specific order"""
-        response = self.tester.post('/api/v1/orders', data = self.order)
+        response = self.tester.post('/api/v1/orders', data=self.order)
         self.assertEqual(201, response.status_code)
-        result = self.tester.get('/api/v1/orders/1', data = self.order )
+        result = self.tester.get('/api/v1/orders/1', data=self.order)
         self.assertEqual(result.status_code, 201)
         self.assertIn('Your order has been received', str(response.data))
         
     def test_post(self):
         """Tests api to place new order"""  
-        response = self.tester.post('/api/v1/orders', data = self.order)
+        response = self.tester.post('/api/v1/orders', data=self.order)
         self.assertEqual(201, response.status_code)
         self.assertIn('Your order has been received', str(response.data))
         
     def test_put(self):
         """Tests api to edit and already existing order"""
-        response = self.tester.post('/api/v1/orders', data = {'orderId':2, 'userId':1, 'foodId':1,'staus':'pending'})
+        response = self.tester.post('/api/v1/orders',
+            data={'orderId': 2, 'userId': 1, 'foodId': 1, 'staus': 'pending'})
         self.assertEqual(201, response.status_code)
 
-        response = self.tester.put('/api/v1/orders/2', data = {'orderId':2,'userId':1, 'foodId':1,'status':'complete'})
+        response = self.tester.put('/api/v1/orders/2',
+            data={'orderId': 2, 'userId': 1, 'foodId': 1, 'status': 'complete'}
+            )
         self.assertEqual(response.status_code, 200)
         self.assertIn('Order status has been updated', str(response.data))
 
     def test_delete(self):
         """Test api to delete a certain order"""
-        response = self.tester.delete('/api/v1/orders/1', data = self.order)
+        response = self.tester.delete('/api/v1/orders/1', data=self.order)
         self.assertEqual(200, response.status_code)
-        response = self.tester.get('/api/v1/orders/1', data = self.order)
+        response = self.tester.get('/api/v1/orders/1', data=self.order)
         self.assertEqual(404, response.status_code)
         
 if __name__ == ('__main__'):
